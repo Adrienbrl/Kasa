b50a524cdf67680e2fa919bf595eca785a007f3a
unknown <bruel.adrienpro@gmail.com>
2023-07-12 18:13:31 +0200

    Mise en place de la navigation dans le header grace a navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Header.scss";
+import { NavLink } from "react-router-dom";
 function Header(props) {
   return (
     <header>
       <nav>
         <div className="Logo">
-          <img src="../../../assets/images/LOGO.svg" alt="Logo Kasa" />
+          <NavLink to="/home">
+            <img src="../../../assets/images/LOGO.svg" alt="Logo Kasa" />
+          </NavLink>
         </div>
         <ul>
-          <li>Accueil</li>
-          <li>A propos</li>
+          <li>
+            <NavLink to="/home">Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">A propos</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
